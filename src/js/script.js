05fc393fe0499b57ae260ae5c2e23858bbbc1e97
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-10-13 12:21:53 -0300

    trazendo para o github

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,71 @@ $("#btnLampOn").click(()=>{
     $("#ligarDesligar").hide();
     $("#btnLampOff").toggle();
 });
+
+//Abrir projeto Android
+
+$("#btnProjetoAndroid").click(()=>{
+    $("#header").toggleClass("blur");
+    $("#btnLampOff").toggleClass("blur");
+    $("#titulo").toggleClass("blur");
+    $("#btnProjetoAndroid").toggleClass("blur");
+    $("#btnProjetoCordel").toggleClass("blur");
+    $("#btnProjetoPokedex").toggleClass("blur");
+    $("#menuAndroid").toggle();
+});
+
+//Fechar Projeto Android
+$("#btnVoltarAndroid").click(()=>{
+    $("#menuAndroid").hide();
+    $("#header").removeClass("blur");
+    $("#btnLampOff").removeClass("blur");
+    $("#titulo").removeClass("blur");
+    $("#btnProjetoAndroid").removeClass("blur");
+    $("#btnProjetoCordel").removeClass("blur");
+    $("#btnProjetoPokedex").removeClass("blur");
+})
+
+
+//Abrir projeto Cordel
+$("#btnProjetoCordel").click(()=>{
+    $("#header").toggleClass("blur");
+    $("#btnLampOff").toggleClass("blur");
+    $("#titulo").toggleClass("blur");
+    $("#btnProjetoAndroid").toggleClass("blur");
+    $("#btnProjetoCordel").toggleClass("blur");
+    $("#btnProjetoPokedex").toggleClass("blur");
+    $("#menuCordel").toggle();
+});
+
+//Fechar projeto Cordel
+$("#btnVoltarCordel").click(()=>{
+    $("#menuCordel").hide();
+    $("#header").removeClass("blur");
+    $("#btnLampOff").removeClass("blur");
+    $("#titulo").removeClass("blur");
+    $("#btnProjetoAndroid").removeClass("blur");
+    $("#btnProjetoCordel").removeClass("blur");
+    $("#btnProjetoPokedex").removeClass("blur");
+});
+
+//Abrir projeto Pokedex
+$("#btnProjetoPokedex").click(()=>{
+    $("#header").toggleClass("blur");
+    $("#btnLampOff").toggleClass("blur");
+    $("#titulo").toggleClass("blur");
+    $("#btnProjetoAndroid").toggleClass("blur");
+    $("#btnProjetoCordel").toggleClass("blur");
+    $("#btnProjetoPokedex").toggleClass("blur");
+    $("#menuPokedex").toggle();
+})
+
+//Fechar projeto Pokedex
+$("#btnVoltarPokedex").click(()=>{
+    $("#menuPokedex").hide();
+    $("#header").removeClass("blur");
+    $("#btnLampOff").removeClass("blur");
+    $("#titulo").removeClass("blur");
+    $("#btnProjetoAndroid").removeClass("blur");
+    $("#btnProjetoCordel").removeClass("blur");
+    $("#btnProjetoPokedex").removeClass("blur");
+});
