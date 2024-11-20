8e0831e2ffd47359671ee1bae82c23fdced1107f
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-11-20 12:46:38 -0300

    removendo os arquivos antigos

--- App/View/home.php
+++ App/View/home.php
@@ -12,93 +12,76 @@
 </head>
 
 <body>
-    <header id="header">
-        <nav class="dropdownMenu">
+    <main>
+        <header>
+            <h1 class="nome">Marcelo Henrique Cornelis Rei</h1>
+        </header>
 
-            <button id="btnMenu"><span class="material-symbols-outlined">menu</span></button>
-            <!-- <ul id="listarMenu">
-                <li><a href="#">Carreira</a></li>
-            </ul> -->
-        </nav>
-        <h1 class="nome">Marcelo Henrique Cornelis Rei</h1>
-    </header>
-    <section class="lamp" id="sectionLamp">
-        
-        <article id="ligarDesligar"><span>
-                
-            </span>
-        </article>
-    </section>
-    
-    
-        <section><img src="../../Assets/img/titlePage.png" alt="título da página" class="titulo" id="titulo"> </section>
-        <section class="dividir-em-dois">
-            <button class="btnProjeto" id="btnProjetoAndroid">
-                <ul class="menuProjetos" id="androidCard">
-                    <img src="../../Assets/img/projetoAndroid.png" alt="projeto android" class="projetos">
-                    <li>Projeto Android</li>
-                </ul>
-            </button>
-            <ul class="menuProjeto" id="menuAndroid">
-                <article class="sectionIframe"><iframe src="https://profissional-marcelo.github.io/Projeto-Android/" frameborder="0"></iframe></article>
-                <article class="container">
-                    <li><a href="src/cursos/html-css(curso-em-video)/projetos-replicados/projeto-android/index.html" target="_blank">Ir para</a></li>
-                    <li>Baixar Script</li>
-                </article>
-                <li><button id="btnVoltarAndroid"><img src="../../Assets/img/voltar.png" alt="botão voltar"></button></li>
-            </ul>
-            <button class="btnProjeto" id="btnProjetoCordel">
-                <ul class="menuProjetos" id="cordelCard">
-                    <img src="../../Assets/img/projetoCordel.png" alt="projeto Cordel" class="projetos">
-                    <li>Projeto Cordel</li>
-                </ul>
-            </button>
-            <ul class="menuProjeto" id="menuCordel">
-                <article class="sectionIframe"><iframe src="https://profissional-marcelo.github.io/Projeto-Cordel/" frameborder="0"></iframe></article>
-                <article class="container">
-                    <li><a href="src/cursos/html-css(curso-em-video)/projetos-replicados/projeto-cordel/index.html" target="_blank">Ir para</a></li>
-                    <li>Baixar Script</li>
-                </article>
-                <li><button id="btnVoltarCordel"><img src="../../Assets/img/voltar.png" alt="botão voltar"></button></li>
-            </ul>
-        </section class="dividir-em-dois">
-        <section class="dividir-em-dois">
-            <button class="btnProjeto" id="btnProjetoPokedex">
-                <ul class="menuProjetos" id="pokedexCard">
-                    <img src="../../Assets/img/projetoPokedex.png" alt="projeto Pokedex" class="projetos">
-                    <li>Projeto Pokédex</li>
-                </ul>
-            </button>
-            <ul class="menuProjeto" id="menuPokedex">
-                <article class="sectionIframe"><iframe src="https://profissional-marcelo.github.io/Projeto-Pokedex/" frameborder="0"></iframe></article>
-                <article class="container">
-                    <li><a href="src/cursos/dev-em-dobro/projetos-replicados/Pokedex/index.html" target="_blank">Ir para</a></li>
-                    <li>Baixar Script</li>
-                </article>
-                <li><button id="btnVoltarPokedex"><img src="../../Assets/img/voltar.png" alt="botão voltar"></button></li>
-            </ul>
-            <button class="btnProjeto" id="btnProjetoRedes-sociais">
-                <ul class="menuProjetos" id="redes-sociaisCard">
-                    <img src="../../Assets/img/projetoRedes-sociais.png" alt="projeto Redes SOciais" class="projetos">
-                    <li>Projeto Redes-Sociais</li>
-                </ul>
-            </button>
-            <ul class="menuProjeto" id="menuRedes-sociais">
-                <article class="sectionIframe"><iframe src="https://profissional-marcelo.github.io/ProjetoRedesSociais/" frameborder="0"></iframe></article>
-                <article class="container">
-                    <li><a href="src/cursos/html-css(curso-em-video)/projetos-replicados/projeto-social/index.html" target="_blank">Ir para</a></li>
-                    <li>Baixar Script</li>
-                </article>
-                <li><button id="btnVoltarRedesSociais"><img src="../../Assets/img/voltar.png" alt="botão voltar"></button></li>
-            </ul>
+        <section class="lamp" id="lamp">
+            <container>
+                <button id="btn_lamp"><img src="../../Assets/img/lampOff.png" alt="Lâmpada desligada" class="lamp" id="lamp_off">
+                    <img src="../../Assets/img/lampOn.png" alt="Lâmpada ligada" class="lamp disable" id="lamp_on">
+                </button>
+                <p class="disable">"Não existe formula mágica para aprender, existe estudo, persistência e dedicação."</p>
+            </container>
         </section>
-      
-    <section class="penguin">
-        <a href="https://www.marcelorei.com.br"><img src="../../Assets/img/penguin.png" alt="pinguim ícone"></a>
-        <p>Venha conhecer minha página!</p>
-    </section>
-    <script src="../../Assets/js/jquery.js"></script>
-    <script src="../../Assets/js/script.js"></script>
+        <section class="projetos">
+            <img src="../../Assets/img/titlePage.png" alt="">
+            <slider>
+                <container class="slides">
+                    <div>
+                        <button class="btnProjeto" id="btnProjetoRedes-sociais" data-src="https://profissional-marcelo.github.io/ProjetoRedesSociais/">
+                            <img src="../../Assets/img/projetoRedes-sociais.png" alt="projeto Redes SOciais" class="projetos">
+                            <p>Projeto Redes-Sociais</p>
+                        </button>
+                        <button class="btnProjeto" id="btnProjetoCordel" data-src="https://profissional-marcelo.github.io/Projeto-Cordel/">
+                            <img src="../../Assets/img/projetoCordel.png" alt="projeto Cordel" class="projetos">
+                            <p>Projeto Cordel</p>
+                        </button>
+                    </div>
+                    <div>
+                        <button class="btnProjeto" id="btnProjetoAndroid" data-src="https://profissional-marcelo.github.io/Projeto-Android/">
+                            <div class="botao_projeto">
+                                <img src="../../Assets/img/projetoAndroid.png" alt="projeto android" class="projetos">
+                                <p>Projeto Android</p>
+                            </div>
+                        </button>
+                        <button class="btnProjeto" id="btnProjetoPokedex" data-src="https://profissional-marcelo.github.io/Projeto-Pokedex/">
+                            <img src="../../Assets/img/projetoPokedex.png" alt="projeto Pokedex" class="projetos">
+                            <p>Projeto Pokédex</p>
+                        </button>
+                    </div>
+                </container>
+            </slider>
+        </section>
+
+        <section class="iframe" style="display: none;">
+            <iframe src="" frameborder="0" id="iframe"></iframe>
+            <div id="botoes">
+                <a href="" id="link_projeto" target="_blank"><button id="ir_para"><span class="material-symbols-outlined">arrow_outward</span></button></a>
+                <button id="fechar_projeto"><span class="material-symbols-outlined">close</span></button>
+                <button id="baixar_codigo"><span class="material-symbols-outlined">download</span></button>
+                <div>
+                    <label name="label" for="ir_para">Ir para</label>
+                    <label name="fechar_projeto" for="fechar_projeto">Fechar</label>
+                    <label for="baixar_codigo">Baixar código</label>
+                </div>
+            </div>
+        </section>
+
+        <section class="penguin">
+            <button id="menu"><img src="../../Assets/img/penguin.png" alt="pinguim ícone"></button>
+        </section>
+
+        <menu class="disable">
+            <ul>
+                <li><a href="#">Carreira</a></li>
+                <li><a href="#">Certificados</a></li>
+                <li><a href="#">Códigos rápidos</a></li>
+                <li><a href="https://www.marcelorei.com.br">Portfólio</a></li>
+            </ul>
+        </menu>
+        <script src="../../Assets/js/home.js"></script>
 </body>
 
 </html>
